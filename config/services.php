0d1bba7f63404aae31ff9e2565a057c805eb0183
Nasir Mehmood <oknasir@gmail.com>
2017-07-03 19:17:31 +0500

    push notifications for android

--- config/services.php
+++ config/services.php
@@ -14,6 +14,10 @@ return [
     |
     */
 
+    'android' => [
+        'key' => env('PUSH_NOTIFICATION_ANDROID_KEY'),
+    ],
+
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
