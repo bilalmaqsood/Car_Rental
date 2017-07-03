2b0d1b7fdcf3172ab785f131a130e80e731a0043
Bilal Maqsood <bilal.maqsood@srhives.com>
2017-07-03 18:51:51 +0500

    QK-30 Save contact us form

--- resources/assets/js/components/ContactForm.vue
+++ resources/assets/js/components/ContactForm.vue
@@ -5,13 +5,15 @@
             <h2>Get in touch</h2>
 
             <ul class="nav nav-tabs" role="tablist">
-                <li role="presentation" class="active"><a href="#contact_us" aria-controls="contact_us" role="tab" data-toggle="tab">
+                <li role="presentation" class="active"><a href="#contact_us" aria-controls="contact_us" role="tab"
+                                                          data-toggle="tab">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="svg-icon">
                         <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#send"></use>
                     </svg>
                     contact us
                 </a></li>
-                <li role="presentation"><a href="#report_issue" aria-controls="report_issue" role="tab" data-toggle="tab">
+                <li role="presentation"><a href="#report_issue" aria-controls="report_issue" role="tab"
+                                           data-toggle="tab">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 20" class="svg-icon">
                         <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#issue"></use>
                     </svg>
@@ -24,8 +26,11 @@
                     FAQs
                 </a></li>
             </ul>
-
             <div class="tab-content">
+                <div v-show="postSuccess" class="alert alert-success" role="alert" ><h4>{{response}}</h4>
+                <div class="clearfix"></div>
+                </div>
+
                 <div role="tabpanel" class="tab-pane active" id="contact_us">
                     <div class="contact_form">
                         <form class="form-inline">
@@ -38,21 +43,26 @@
                                 </div>
                                 <div class="form-group" :class="{ 'has-error': $v.form.email.$error }">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 29 25" class="svg-icon">
-                                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#form_envelope"></use>
+                                        <use xmlns:xlink="http://www.w3.org/1999/xlink"
+                                             xlink:href="#form_envelope"></use>
                                     </svg>
-                                    <input type="email" class="form-control" placeholder="e-mail" v-model.trim="form.email">
+                                    <input type="email" class="form-control" placeholder="e-mail"
+                                           v-model.trim="form.email">
                                 </div>
                                 <div class="form-group" :class="{ 'has-error': $v.form.phone.$error }">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 25" class="svg-icon">
                                         <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#mobile"></use>
                                     </svg>
-                                    <input type="text" class="form-control" placeholder="phone number" v-model.trim="form.phone">
+                                    <input type="text" class="form-control" placeholder="phone number"
+                                           v-model.trim="form.phone">
                                 </div>
                                 <div class="form-group" :class="{ 'has-error': $v.form.subject.$error }">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 20" class="svg-icon">
-                                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#sign_contract_icon"></use>
+                                        <use xmlns:xlink="http://www.w3.org/1999/xlink"
+                                             xlink:href="#sign_contract_icon"></use>
                                     </svg>
-                                    <input type="text" class="form-control" placeholder="subject" v-model="form.subject">
+                                    <input type="text" class="form-control" placeholder="subject"
+                                           v-model="form.subject">
                                 </div>
                             </div>
                             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
@@ -68,12 +78,14 @@
                     </div>
                 </div>
                 <div role="tabpanel" class="tab-pane" id="report_issue">
-                    <p>Nunc volutpat vehicula erat at facilisis. Quisque congue et turpis non maHs. Quisque egestas eleifend purus, eu finibus magna. In cursus fringilla leo consequat sollicitudin. Nunc in massa orci. Aenean pulvinar egestas rutrum. Aliquam elit libero, porta ac neque ut, fringilla laoreet lectus. Sed ut metus vitae risus iaculis vulputate. Curabitur preEum turpis nec velit commodo, nec egestas quam fringilla. Cras molesEe turpis eu lacus fringilla sodales. EEam a vesEbulum
+                    <p>
+                        Nunc volutpat vehicula erat at facilisis. Quisque congue et turpis non maHs. Quisque egestas eleifend purus, eu finibus magna. In cursus fringilla leo consequat sollicitudin. Nunc in massa orci. Aenean pulvinar egestas rutrum. Aliquam elit libero, porta ac neque ut, fringilla laoreet lectus. Sed ut metus vitae risus iaculis vulputate. Curabitur preEum turpis nec velit commodo, nec egestas quam fringilla. Cras molesEe turpis eu lacus fringilla sodales. EEam a vesEbulum
                         felis.
                         Suspendisse laoreet dignissim nibh in posuere. Aenean a nisl dignissim, hendrerit magna vitae, interdum nisi. Suspendisse volutpat tortor vel libero dignissim, a viverra ex gravida.</p>
                 </div>
                 <div role="tabpanel" class="tab-pane" id="faqs_tab">
-                    <p>Nunc volutpat vehicula erat at facilisis. Quisque congue et turpis non maHs. Quisque egestas eleifend purus, eu finibus magna. In cursus fringilla leo consequat sollicitudin. Nunc in massa orci. Aenean pulvinar egestas rutrum.</p>
+                    <p>
+                        Nunc volutpat vehicula erat at facilisis. Quisque congue et turpis non maHs. Quisque egestas eleifend purus, eu finibus magna. In cursus fringilla leo consequat sollicitudin. Nunc in massa orci. Aenean pulvinar egestas rutrum.</p>
                 </div>
             </div>
 
@@ -88,16 +100,18 @@
     export default {
 
         data() {
-          return {
-              form: {
-                  name: '',
-                  phone: '',
-                  subject: '',
-                  message: '',
-                  email: '',
-              }
+            return {
+                form: {
+                    name: '',
+                    phone: '',
+                    subject: '',
+                    message: '',
+                    email: '',
+                },
+                postSuccess: false,
+                response: "",
 
-          }
+            }
         },
 
         validations: {
@@ -111,11 +125,9 @@
                     minLength: minLength(6)
                 },
                 phone: {
-                    required,
                     minLength: minLength(6)
                 },
                 subject: {
-                    required,
                     minLength: minLength(3)
                 },
                 message: {
@@ -128,12 +140,13 @@
 
         },
 
-        methods:{
+        methods: {
             postForm(){
-                alert("you clicked me??");
                 axios.post('/api/contact', this.form)
                     .then(response => {
-
+                        console.log(response);
+                        this.response=response.data.success;
+                        this.postSuccess=true;
                     });
             }
         }
