d9e948bf7056dbce8803f728370a83a8442beab5
Bilal Maqsood <bilal.maqsood@srhives.com>
2017-07-14 16:23:46 +0500

    Show details of selected vehicle

--- resources/assets/js/components/Vehicles.vue
+++ resources/assets/js/components/Vehicles.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="vehicles_container">
         <div class="car_detail_container">
-            <div class="detail_img">
-                <img src="/images/car_img.png" alt="">
+            <div class="detail_img" v-if="vehicle.images">
+                <img :src="vehicle.images[0]" alt="">
             </div>
             <div class="booking_tab">
                 <ul class="nav nav-tabs">
@@ -35,57 +35,58 @@
                 <vehicle-input-form></vehicle-input-form>
             </div>
 
-            <div class="car_detail">
+            <div class="car_detail" v-if="vehicle">
                 <div class="availablity_detail">
-                    <h3>Toyota Prius 1.8 Hybrid</h3>
+                    <h3>{{vehicle.make}} {{vehicle.model}} {{vehicle.variant}}</h3>
                     <ul>
                         <li>
-                            <p>Year: 2015 </p>
-                            <p>Mileage: 35,044</p>
-                            <p>Seats: 5 </p>
-                            <p>Transmission: manual</p>
+                            <p>Year: {{vehicle.year}} </p>
+                            <p>Mileage: {{vehicle.mileage}}</p>
+                            <p>Seats: {{vehicle.seats}}</p>
+                            <p>Transmission: {{vehicle.transmission}}</p>
                         </li>
                         <li>
                             <p>Seats: 5 </p>
                             <p>Transmission: manual</p>
                         </li>
                         <li>
-                            <p>Fuel type: hybrid </p>
-                            <p>Consumption: 95.2 mpg (ec.)</p>
+                            <p>Fuel type: {{vehicle.fuel}} </p>
+                            <p>Consumption: {{vehicle.mpg_eco}} mpg (ec.)</p>
                         </li>
                     </ul>
                     <div class="availabe">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 15" class="svg-icon">
                             <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#availability_results"></use>
                         </svg>
-                        <p>available from: <span>16.05.2017</span></p>
+                        <p>available from: <span>{{vehicle.available_from | date('fromNow')}}</span></p>
                     </div>
                 </div>
                 <div class="availablity_price">
                     <div class="availabe_item_price">
-                        <h3>£ 320</h3>
+                        <h3>{{vehicle.rent | currency}}</h3>
                         <span>/week</span>
-                        <span>insurance included</span>
+                        <span v-if="vehicle.insurance>0.0">insurance included</span>
                     </div>
                 </div>
             </div>
-            <div class="pickup_loction">
+            <div class="pickup_loction" v-if="vehicle">
                 <button class="secodery_btn">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 20" class="svg-icon">
                         <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#lcotion_icon"></use>
                     </svg>
                 </button>
                 <ul>
-                    <li><span>Pickup from:</span> 45, King William Street, London, EC4r 9AN, UK</li>
-                    <li><span>Return to:</span> 45, King William Street, London, EC4r 9AN, UK</li>
+                    <li><span>Pickup from:</span> <span class="pickup_location">{{ fetchAddress(vehicle.pickup_location)}}</span></li>
+                    <li><span>Return to:</span> <span class="return_location">{{fetchAddress(vehicle.return_location)}}</span></li>
                 </ul>
             </div>
-            <div class="pickup_loction_map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4825851.352956702!2d-4.064941!3d53.800650999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1495513518623" width="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <div class="pickup_loction_map" v-if="vehicle">
+                <iframe width="100%" height="450" frameborder="0" style="border:0" :src="'https://www.google.com/maps/embed/v1/place?q='+vehicle.location.split(',')[0]+','+vehicle.location.split(',')[1]+'&amp;key=AIzaSyDFkedYDgj286xDo9Sp9XRWsOiPfu9T3Ak'"></iframe>
                 <p>Phasellus nisi leo, aliquet ac erat ut, moles2e ma3s enim. Proin sit amet tempor mi, a egestas tortor. Ves2bulum et congue urna. Nunc elementum por3tor
                     urna sed euismod. Curabitur bibendum leo iaculis pulvinar fringilla. Nullam eu interdum lectus. Duis posuere, enim at fermentum efficitur, magna nisl lobor2s
                     metus, in hendrerit est ante et magna. Ut ma3s, justo non por3tor suscipit, augue ipsum sodales justo, a rutrum.</p>
             </div>
+
             <div class="pickup_loction_datebox">
                 <div class="availabe">
                     <p>John Doe <span>16.05.2017</span></p>
@@ -93,6 +94,29 @@
                 <div class="ratting" data-score="1"></div>
             </div>
 
+            <div class="pofile_content_wrapper" v-for="vehicle in vehicles">
+                <div v-if="vehicle.images" class="clickable img_box" v-bind:style="{'background': 'url('+vehicle.images[0]+')'}" @click="vechicleDetails(vehicle)">
+                    <img :src="vehicle.images[0]" alt="">
+                </div>
+                <div class="profile_content">
+                    <h3 @click="vechicleDetails(vehicle)" class="clickable"> {{vehicle.make}} {{vehicle.model}} {{vehicle.variant}}</h3>
+                    <ul>
+                        <li>
+                            <p>Year:  {{vehicle.year}}</p>
+                            <p>Mileage: {{vehicle.mileage}}</p>
+                        </li>
+                        <li>
+                            <p>Seats: {{vehicle.seats}} </p>
+                            <p>Transmission: {{vehicle.transmission}}</p>
+                        </li>
+                        <li>
+                            <p>Fuel type: {{vehicle.fuel}} </p>
+                            <p>Consumption:  {{vehicle.mpg}} mpg (ec.)</p>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
 
         </div>
     </div>
@@ -103,9 +127,9 @@
         data() {
             return {
                 vehicles: '',
+                vehicle: false,
             };
         },
-
         mounted() {
             let $scope = this;
             this.prepareComponent();
@@ -118,10 +142,31 @@
             },
             vehiclesCallback(r) {
                     this.vehicles = r.data.success;
+                    if(r.data.success[0]){
+                        this.vehicle = r.data.success[0];
+                    }
 
+            },
+            fetchAddress(arg){
+                console.log(arg);
+                let $t = this;
+                let location;
+                    $.ajax({
+                        url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + arg + '&key=AIzaSyDp8Pjc5ZmcmTb-ci-Fj-xNh2KLTUlguk0',
+                        type: 'GET',
+                        dataType: 'json',
+                        async: false,
+                    }).done(function (r) {
+                        location = r.results[0].formatted_address;
+                    });
+
+            return location;
             },
             date_format(date){
                 return moment.utc(date.date).format("D.M.Y");
+            },
+            vechicleDetails(arg){
+                this.vehicle = arg;
             }
         }
     }
