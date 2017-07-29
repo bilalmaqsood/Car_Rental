dd11663d381b50a875e07d08d8bc076bf9216df1
Nasir Mehmood <oknasir@gmail.com>
2017-07-29 16:14:19 +0500

    update for migration

--- database/migrations/2017_07_29_102226_update_booking_table_for_account.php
+++ database/migrations/2017_07_29_102226_update_booking_table_for_account.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateBookingTableForPayment extends Migration
+class UpdateBookingTableForAccount extends Migration
 {
     /**
      * Run the migrations.
