b0fff5cf19314e59023a7d1322a4d59eb5aec033
Bilal Maqsood <bilal.maqsood@srhives.com>
2017-08-09 10:38:05 +0500

    listing documents of vehicles

--- resources/views/admin/vehicles/show.blade.php
+++ resources/views/admin/vehicles/show.blade.php
@@ -12,42 +12,67 @@
                                     <div class="panel-body mainDescription">
                                             <div class="row">
                                                 <div class="col-xs-3">
-                                                    <label><b>User name:</b></label>
+                                                    <label><b>Make:</b></label>
                                                 </div>
                                                 <div class="col-xs-9">
-                                                    {{ $user->name }}
+                                                    {{ $vehicle->make }}
                                                 </div>
                                              </div>
                                             <div class="row">
                                                 <div class="col-xs-3">
-                                                    <label><b>Email:</b></label>
+                                                    <label><b>Model:</b></label>
                                                 </div>
                                                 <div class="col-xs-9">
-                                                    {{ $user->email }}
+                                                    {{ $vehicle->model }}
                                                 </div>
                                             </div>
                                             <div class="row">
                                                 <div class="col-xs-3">
-                                                    <label><b>Type:</b></label>
+                                                    <label><b>variant:</b></label>
                                                 </div>
                                                 <div class="col-xs-9">
-                                                    {{ $user_type }}
+                                                    {{ $vehicle->variant }}
                                                 </div>
                                             </div>
                                             <div class="row">
                                                 <div class="col-xs-3">
-                                                    <label><b>Phone:</b></label>
+                                                    <label><b>Year:</b></label>
                                                 </div>
                                                 <div class="col-xs-9">
-                                                    {{ $user->phone }}
+                                                    {{ $vehicle->year }}
                                                 </div>
                                             </div>
+                                            @if($vehicle->documents)
+                                            <div class="row">
+                                                <h3>Vehicle documents</h3>
+
+                                                @foreach($vehicle->documents as $document)
+                                                <div class="panel panel-default">
+                
+                                                   <div class="panel-heading" role="tab" id="headingThree">
+                                                        <h4 class="panel-title">
+                                                            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#{{ preg_replace('/\s+/', '', $document['name']) }}" aria-expanded="false" aria-controls="collapseThree">
+                                                                {{ $document['name'] }}
+                                                            </a>
+                                                        </h4>
+                                                    </div>
+                                                    <div id="{{ preg_replace('/\s+/', '', $document['name']) }}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                                                        <div class="panel-body">
+                                                            <img src="{{ $document['path'] }}" alt="">
+                                                        </div>
+                                                    </div>
+                                               </div>
+                                                @endforeach
+                                            </div>
+                                            @endif
+
+
                                     </div>
                                 </div>
 
                                 <div class="row">
                                     <div class="col-md-12">
-                                        <a href="{{ route('users.index') }}" class="btn btn-default btn-wide pull-right">cancel</a>
+                                        <a href="{{ route('vehicles.index') }}" class="btn btn-default btn-wide pull-right">cancel</a>
                                     </div>
                                 </div>
                             </div>
