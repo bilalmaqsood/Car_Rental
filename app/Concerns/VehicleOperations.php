d82a5683e3ff622e31b4c5fca0d92d24dfe20ce5
bilalmaqsood-srhives <bilal.maqsood@srhives.com>
2017-08-25 17:06:20 +0500

    Fixing document verification check

--- app/Concerns/VehicleOperations.php
+++ app/Concerns/VehicleOperations.php
@@ -8,7 +8,7 @@ trait VehicleOperations
     {
         $error = array();
         foreach (request()->documents as $key => $doc){
-            if(isset($doc['doc']) && isset($doc['title']) && $doc['path']==null){
+            if(isset($doc['doc']) && $doc['path']==null){
             array_push($error, $doc["title"]. " is missing");
             }
         }
