ef9ec42d692313197d65ed84e4778f7eaecba98d
bilalmaqsood-srhives <bilal.maqsood@srhives.com>
2017-08-29 14:58:26 +0500

    Adding return deposit command to the schedular

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,10 +25,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
         $schedule->command('reset:deposit')
                  ->daily();
+        $schedule->command('weekly:deduction')
+                 ->daily();
+
     }
 
     /**
