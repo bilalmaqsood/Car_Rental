722090fb263cff9eb9938417503a11fc6912943b
bilalmaqsood-srhives <bilal.maqsood@srhives.com>
2017-08-29 11:10:55 +0500

    Fixing phone no validation

--- app/Http/Requests/UserModel.php
+++ app/Http/Requests/UserModel.php
@@ -51,7 +51,7 @@ class UserModel extends FormRequest
                 return [
                     'name' => 'required|alpha_spaces',
                     'email' => 'required|email',
-                    'phone' => 'numeric|min:11',
+                    'phone' => 'required|min:11',
                     'postcode' => [
                         'Regex:/^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][‌​0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$/'
                     ],
