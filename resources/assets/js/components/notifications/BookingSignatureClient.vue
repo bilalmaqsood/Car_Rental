11faaebeb1eb5a5f5b884f94c3bb711da64bc1f6
Bilal Maqsood <bilal.maqsood@srhives.com>
2017-10-12 17:40:40 +0500

    fixing notification

--- resources/assets/js/components/notifications/BookingSignatureClient.vue
+++ resources/assets/js/components/notifications/BookingSignatureClient.vue
@@ -2,15 +2,6 @@
     <div class="booking-request-actions notification-shadow">
         <div class="inlane-btn-wrap inlane-btn-wrap-btn2">
             <ul class="two-btn-inlane">
-
-                <!-- <li>
-                    <a  href="javascript:void(0)"  @click="approveEvent">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" class="svg-icon">
-                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#hellp"></use>
-                        </svg>
-                        approve
-                    </a>
-                </li> -->
                 <li>
                 <a  href="javascript:void(0)"  @click="contractEvent">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 25" class="svg-icon">
@@ -32,10 +23,10 @@
         <div class="btn-inlane-content btn-inlane-content-btn3 noty_successfull">
             <div class="driver-profile-text">
                 <h3>Contract signed</h3>
-                 <p><b>{{notification.data.user}}</b>has signed the contract for the booking of {{ notification.data.vehicle }}
+                 <p><b>{{notification.data.user}}</b> has signed the contract for the booking of {{ notification.data.vehicle }}
                  </p>
                  <p><b>Contract start:</b> {{notification.data.contract_start.date | date('format', 'DD.MM.YYYY') }} </p>
-                <p><b>Contract end:</b> {{notification.data.contract_end.date | date('format', 'DD.MM.YYYY') }}</p>
+                <p><b>Contract end:</b> {{notification.data.contract_end.date | date('format', 'DD.MM.YYYY') }}</p> <br>
                 <p>
                 You can now check and sign the contract and set your <b>Direct Debit</b>.</p>
             </div>
--- resources/assets/js/components/notifications/BookingSignatureOwner.vue
+++ resources/assets/js/components/notifications/BookingSignatureOwner.vue
@@ -1,21 +1,7 @@
 <template>
     <div class="booking-request-actions notification-shadow">
-        <div class="inlane-btn-wrap inlane-btn-wrap-btn3">
-            <ul class="three-btn-inlane">
-                <li>
-                    <a href="javascript:void(0)" @click="viewProfileEvent">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 25" class="svg-icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#user"></use></svg>
-                        view driver
-                    </a>
-                </li>
-                <!-- <li>
-                    <a  href="javascript:void(0)"  @click="approveEvent">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" class="svg-icon">
-                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#hellp"></use>
-                        </svg>
-                        approve
-                    </a>
-                </li> -->
+        <div class="inlane-btn-wrap inlane-btn-wrap-btn2">
+            <ul class="two-btn-inlane">
                 <li>
                 <a  href="javascript:void(0)"  @click="contractEvent">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 25" class="svg-icon">
@@ -25,9 +11,11 @@
                 </a>
                 </li>
                 <li>
-                    <a  href="javascript:void(0)" @click="declineEvent">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" class="svg-icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#close_icon"></use></svg>
-                        decline
+                    <a href="javascript:void(0)">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 20" class="svg-icon">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#chat"></use>
+                        </svg>
+                        chat
                     </a>
                 </li>
             </ul>
@@ -35,10 +23,10 @@
         <div class="btn-inlane-content btn-inlane-content-btn3 noty_successfull">
             <div class="driver-profile-text">
                 <h3>Contract signed</h3>
-                 <p><b>{{notification.data.user}}</b>has signed the contract for the booking of {{ notification.data.vehicle }}
+                 <p><b>{{notification.data.user}} </b>has signed the contract for the booking of {{ notification.data.vehicle }}
                  </p>
                  <p><b>Contract start:</b> {{notification.data.contract_start.date | date('format', 'DD.MM.YYYY') }} </p>
-                <p><b>Contract end:</b> {{notification.data.contract_end.date | date('format', 'DD.MM.YYYY') }}</p>
+                <p><b>Contract end:</b> {{notification.data.contract_end.date | date('format', 'DD.MM.YYYY') }}</p> <br>
                 <p>
                 You can now check and sign the contract and set your <b>Direct Debit</b>.</p>
             </div>
