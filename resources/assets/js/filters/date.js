fe106d8b14aacbfb56208f6595c1bd831c3d5d80
Bilal Maqsood <bilal.maqsood@srhives.com>
2017-10-24 11:09:25 +0500

    QK-227 fixing available from

--- resources/assets/js/filters/date.js
+++ resources/assets/js/filters/date.js
@@ -1,3 +1,7 @@
 module.exports = function (value, type, format) {
+    console.log(moment().diff(value));
+    if(typeof type !== 'undefined' && moment().diff(value) < 86400000  &&  moment().diff(value)>0) //39651088
+        return 'just now';
+        else
     return typeof type !== 'undefined' ? (typeof type !== 'undefined' ? moment.utc(value)[type](format) : moment.utc(value)[type]()) : moment.utc(value).format('dddd, MMMM Do YYYY, h:mm:ss a');
 };
