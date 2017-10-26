b9ef943f1cc729f77ccd237ca4d49db4d99a074a
Bilal Maqsood <bilal.maqsood@srhives.com>
2017-10-26 14:31:13 +0500

    fixing early cancelation issue

--- resources/assets/js/components/EarlyCancelation.vue
+++ resources/assets/js/components/EarlyCancelation.vue
@@ -40,7 +40,7 @@ import User from '../user';
 
         mounted() {
             let $this = this; 
-            this.fetchBookedSlots();
+            
             $('#CancelDate').datetimepicker({
                     inline: true,
                     sideBySide: false,
@@ -48,6 +48,7 @@ import User from '../user';
                 }).on('dp.change', this.calenderChange)
                   .on('dp.update', function(){ $this.highlightOldDays($this.booked_slots) });
                 $('[data-toggle="tooltip"]').tooltip();
+                this.fetchBookedSlots();
 
         },
 
@@ -132,7 +133,7 @@ import User from '../user';
                     this.total_slots = r.data.success.totalSlots;
                     this.booked_slots = r.data.success.bookedSlots;
                     this.start_date = moment(r.data.success.firstDay);;
-                    this.highlightOldDays(r.data.success.booked_slots);
+                    this.highlightOldDays(r.data.success.bookedSlots);
                 });
             },
             highlightDays(bool) {
