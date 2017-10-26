900cbc259360c51f10702cc957d6bd97017b4277
Bilal Maqsood <bilal.maqsood@srhives.com>
2017-10-26 19:01:54 +0500

    fixing booking notifications

--- resources/assets/js/components/notifications/BookingAccepted.vue
+++ resources/assets/js/components/notifications/BookingAccepted.vue
@@ -1,11 +1,34 @@
 <template>
     <div class="booking-request-actions notification-shadow">
+            <div class="inlane-btn-wrap inlane-btn-wrap-btn2">
+            <ul class="two-btn-inlane">
+                <li>
+                <a  href="javascript:void(0)"  @click="contractEvent">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 25" class="svg-icon">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#booking_menu"></use>
+                    </svg>
+                    contract
+                </a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" @click="chatEvent">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 20" class="svg-icon">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#chat"></use>
+                        </svg>
+                        chat
+                    </a>
+                </li>
+            </ul>
+        </div>
         <div class="btn-inlane-content btn-inlane-content-btn3 noty_successfull">
             <div class="driver-profile-text">
             <h3>Booking request accepted</h3>
             <p>{{ notification.data.user}} approved your request to book <b> {{notification.data.vehicle}} </b>
-            <p><b>Contract start:</b> {{notification.data.contract_start.date | date('format', 'DD.MM.YYYY') }} </p>
-            <p><b>Contract end:</b> {{notification.data.contract_end.date | date('format', 'DD.MM.YYYY') }}</p>
+
+            <ul>
+                <li><b>Contract start:</b> {{notification.data.contract_start.date | date('format', 'DD.MM.YYYY') }} </li>
+                <li><b>Contract end:</b> {{notification.data.contract_end.date | date('format', 'DD.MM.YYYY') }}</li>
+            </ul>
             You can now check and sign the contract and set your Direct Debit. A
             deposit of <b>{{notification.data.deposit | currency}}</b> have been taken from your card ending in <b>{{notification.data.card_ending}}</b></p>
         </div>
@@ -33,6 +56,25 @@
         },
 
         methods: {
+
+            viewProfileEvent(){
+                this.$parent.$emit("viewPofile",{booking_id: this.notification.data.id});
+            },
+            approveEvent(){
+                this.$parent.$emit("approve",this.notification);
+
+            },
+            declineEvent(){
+                this.$parent.$emit("decline",this.notification);
+
+            },
+            contractEvent(){
+                this.$parent.$emit("contract",this.notification);
+
+            },
+            chatEvent(){
+                this.$parent.$emit("chat",this.notification);
+            }
 
         }
     }
